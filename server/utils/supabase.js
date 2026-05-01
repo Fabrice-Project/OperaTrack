80b59e388d24a6932628eabd176d8e88f519a399
Fabrice-Project <deviennefabrice@gmail.com>
2026-05-01 11:35:04 +0200

    fix: SUPABASE_SERVICE_KEY -> SUPABASE_SERVICE_ROLE_KEY

--- server/utils/supabase.js
+++ server/utils/supabase.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
   { auth: { autoRefreshToken: false, persistSession: false } }
 );
 
